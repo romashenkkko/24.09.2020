cdce0b236b0a27da2bd05c6a850beb765201750b
romashenkkko <70690141+romashenkkko@users.noreply.github.com>
2020-09-24 17:51:43 +0300

    Add files via upload

---
+++ exerc5.py
@@ -0,0 +1,2 @@
+import math
+print (2*(math.sqrt(3*(math.sqrt((13+4)*(math.sqrt(3)))))))
---
+++ exerc7.py
@@ -0,0 +1,2 @@
+import math
+print (math.sqrt((26+6)*math.sqrt((13-4)*math.sqrt((8+2)*2)*math.sqrt((6-2)*math.sqrt(5))))+(math.sqrt((26-6)*math.sqrt((13+4)*math.sqrt((8-2)*math.sqrt((6+2)*math.sqrt(5)))))))
